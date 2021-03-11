5ac1206eaf1733fc0a58adf817d5a969c8db9e5b
naoto hamada <hamada@vis-its.com>
2021-03-12 00:56:06 +0900

    Fix test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -17,11 +17,11 @@ RSpec.describe "/users", type: :request do
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { name: Faker::Name.name }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { name: '' }
   }
 
   # This should return the minimal set of values that should be in the headers
@@ -85,7 +85,7 @@ RSpec.describe "/users", type: :request do
   describe "PATCH /update" do
     context "with valid parameters" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { name: Faker::Name.name }
       }
 
       it "updates the requested user" do
@@ -93,7 +93,7 @@ RSpec.describe "/users", type: :request do
         patch user_url(user),
               params: { user: new_attributes }, headers: valid_headers, as: :json
         user.reload
-        skip("Add assertions for updated state")
+        expect(user.name).to eq new_attributes[:name]
       end
 
       it "renders a JSON response with the user" do
