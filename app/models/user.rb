fb45e1ad286f19f5d9acf462f32b4cb5109f271d
naoto hamada <hamada@vis-its.com>
2021-03-25 00:25:20 +0900

    Add enum for locale

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  enum locale: {
+    en: 0,
+    ja: 1,
+  }
+
   validates :name, presence: true
   validate :ng_name
+  validates :locale, presence: true, inclusion: { in: locales.keys }
 
   private
 
