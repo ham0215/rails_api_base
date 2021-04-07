fc56345bfc8517d5a63decef9c6542b9017d4611
naoto hamada <hamada@vis-its.com>
2021-04-08 01:31:39 +0900

    WIP

--- app/services/send_grid_service.rb
+++ app/services/send_grid_service.rb
@@ -8,5 +8,13 @@ class SendGridService
   end
 
   def deliver!(mail)
+    from = SendGrid::Email.new(email: 'test@example.com')
+    to = SendGrid::Email.new(email: 'test@example.com')
+    subject = mail.subject
+    content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+    sg_mail = SendGrid::Mail.new(from, subject, to, content)
+
+    sg = SendGrid::API.new(api_key: api_key)
+    response = sg.client.mail._('send').post(request_body: sg_mail.to_json)
   end
 end
