3972ffd4f364761ca63c9044750352fc402a080f
naoto hamada <hamada@vis-its.com>
2021-04-11 23:07:10 +0900

    Fix mail body

--- app/services/send_grid_service.rb
+++ app/services/send_grid_service.rb
@@ -8,14 +8,25 @@ class SendGridService
   end
 
   def deliver!(mail)
-    byebug
-    from = SendGrid::Email.new(email: 'test@example.com')
-    to = SendGrid::Email.new(email: 'test@example.com')
-    subject = mail.subject
-    content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-    sg_mail = SendGrid::Mail.new(from, subject, to, content)
+    sg_mail = SendGrid::Mail.new
+    sg_mail.from = SendGrid::Email.new(email: mail.from.first)
+    sg_mail.to = SendGrid::Email.new(email: mail.to.first)
+    sg_mail.subject = mail.subject
+
+    # html & text
+    sg_mail.add_content(SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby'))
+=begin
+    mail.body.parts.each do
+      content_type = "#{_1.main_type}/#{_1.sub_type}"
+      sg_mail.add_content(SendGrid::Content.new(type: content_type, value: _1.body.raw_source))
+    end
+=end
 
     sg = SendGrid::API.new(api_key: api_key)
     response = sg.client.mail._('send').post(request_body: sg_mail.to_json)
+    byebug
+    # エラー処理
+    raise response.inspect if response.status_code.to_i >= 300
+    response.body
   end
 end
