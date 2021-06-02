0965dfe8e484afddc37e9a33bcdd906bdcbc8a59
naoto hamada <hamada@vis-its.com>
2021-06-02 18:16:24 +0900

    Fix rubocop

--- app/graphql/sources/active_record_object.rb
+++ app/graphql/sources/active_record_object.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
-class Sources::ActiveRecordObject < GraphQL::Dataloader::Source
-  def initialize(model_class, association_name)
-    @model_class = model_class
-    @association_name = association_name
-  end
+module Sources
+  class ActiveRecordObject < GraphQL::Dataloader::Source
+    def initialize(model_class, association_name)
+      super()
+      @model_class = model_class
+      @association_name = association_name
+    end
 
-  def fetch(ids)
-    records = @model_class.where(id: ids).preload(@association_name)
-    ids.map {|id| records.find { _1.id == id.to_i }&.public_send(@association_name) }
+    def fetch(ids)
+      records = @model_class.where(id: ids).preload(@association_name)
+      ids.map { |id| records.find { _1.id == id.to_i }&.public_send(@association_name) }
+    end
   end
 end
