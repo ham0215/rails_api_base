e74f4365b6e83bb2d9248b2ec06f926919aa5123
naoto hamada <hamada@vis-its.com>
2021-06-03 09:59:34 +0900

    Fix to order

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,10 @@ class User < ApplicationRecord
 
   has_one :profile, dependent: :destroy
   has_many :portfolios, dependent: :delete_all
+  has_many :sorted_portfolios, -> { order(id: :desc) }, class_name: 'Portfolio', dependent: :delete_all
   has_many :user_books, dependent: :delete_all
   has_many :books, through: :user_books
+  has_many :sorted_books, -> { order(id: :desc) }, through: :user_books, source: :book
 
   validates :name, presence: true
   validate :ng_name
@@ -23,11 +25,11 @@ class User < ApplicationRecord
   end
 
   def portfolios_loader
-    AssociationLoader.for(User, :portfolios).load(self)
+    AssociationLoader.for(User, :sorted_portfolios).load(self)
   end
 
   def books_loader
-    AssociationLoader.for(User, :books).load(self)
+    AssociationLoader.for(User, :sorted_books).load(self)
   end
 
   private
