cd5dbd093826ede0c7bbdcff344d394c547877b0
naoto hamada <hamada@vis-its.com>
2021-06-11 15:56:07 +0900

    Add uploadBookImage mutation

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -4,7 +4,7 @@ module Mutations
   class BaseMutation < GraphQL::Schema::RelayClassicMutation
     argument_class Types::BaseArgument
     field_class Types::BaseField
-    input_object_class Types::BaseInputObject
+    input_object_class InputObjects::BaseInputObject
     object_class Types::BaseObject
   end
 end
