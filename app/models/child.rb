9edb635c47d337c0b6c25485485077434df2eaac
naoto hamada <hamada@vis-its.com>
2021-06-25 14:58:55 +0900

    Add test

--- app/models/child.rb
+++ app/models/child.rb
@@ -3,6 +3,6 @@
 class Child < ApplicationRecord
   belongs_to :parent
 
-  after_create -> { parent.status = 1 }
-  after_destroy -> { parent.status = 0 }
+  after_create -> { parent.update!(status: 1) }
+  after_destroy -> { parent.update!(status: 0) }
 end
--- spec/factories/children.rb
+++ spec/factories/children.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :child do
-    
+    association :parent
   end
 end
