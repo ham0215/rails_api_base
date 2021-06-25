a80c0bc44e1b8d3636d5679ba532911c8575aee0
naoto hamada <hamada@vis-its.com>
2021-06-25 12:42:22 +0900

    Migrate

---
+++ app/models/child.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Child < ApplicationRecord
+  belongs_to :parent
+
+  after_create -> { parent.status = 1 }
+  after_destroy => { parent.status = 0 }
+end
---
+++ spec/factories/parents.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :parent do
+    
+  end
+end
