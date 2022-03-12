ae4dbd778227519efff4eb37e29cebd141325688
naoto hamada <hamada@vis-its.com>
2022-03-12 21:14:00 +0900

    srb init

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'graphql'
 gem 'graphql-batch'
 gem 'apollo_upload_server'
 
+gem 'sorbet-runtime'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
@@ -43,6 +45,8 @@ group :development do
   gem 'graphiql-rails'
 
   gem 'sprockets-rails', require: 'sprockets/railtie'
+
+  gem 'sorbet'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/models/child_spec.rb
+++ spec/models/child_spec.rb
@@ -11,14 +11,12 @@ RSpec.describe Child, type: :model do
 
     # こちらは失敗する
     it 'statusが0に更新されること' do
-      expect { subject }.to change { Child.exists?(id: child.id) }.from(true).to(false).
-        and change { Parent.find(parent.id).status }.from(1).to(0)
+      expect { subject }.to change { Child.exists?(id: child.id) }.from(true).to(false).and change { Parent.find(parent.id).status }.from(1).to(0)
     end
 
     # こちらは成功する
     it 'statusが0に更新されること' do
-      expect { subject }.to change { Parent.find(parent.id).status }.from(1).to(0).
-        and change { Child.exists?(id: child.id) }.from(true).to(false)
+      expect { subject }.to change { Parent.find(parent.id).status }.from(1).to(0).and change { Child.exists?(id: child.id) }.from(true).to(false)
     end
   end
 end
