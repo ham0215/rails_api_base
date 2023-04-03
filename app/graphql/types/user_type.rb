616bb70f03fd2842a9b8e4292d9270c3ffed3548
Naoto Hamada <naoto.hamada@findy.co.jp>
2023-04-04 01:05:32 +0900

    Refactor loader

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,5 +6,20 @@ module Types
     field :name, String, null: false
     field :email, String, null: false
     field :nickname, String, null: false
+    field :books, Types::BookType.connection_type, null: false
+    field :profile, Types::ProfileType, null: false
+    field :portfolios, Types::PortfolioType.connection_type, null: false
+
+    def books
+      AssociationLoader.for(User, :sorted_books).load(object)
+    end
+
+    def profile
+      AssociationLoader.for(User, :profile).load(object)
+    end
+
+    def portfolios
+      AssociationLoader.for(User, :sorted_portfolios).load(object)
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,23 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     nickname { Faker::Name.name }
+
+    trait :with_profile do
+      after(:create) do |user|
+        create(:profile, user:)
+      end
+    end
+
+    trait :with_books do
+      after(:create) do |user|
+        create_list(:user_book, 2, user:)
+      end
+    end
+
+    trait :with_portfolios do
+      after(:create) do |user|
+        create_list(:portfolio, 2, user:)
+      end
+    end
   end
 end
